28824fc3e7a6b585a2a2de60f0a366ed326ea69b
Felix Kunde <fkunde@virtualcitysystems.de>
2015-02-20 08:41:16 +0100

    [kml-exporter] forgot the "lod[x]_geometry is not null" condition in last change of queries

--- src/de/tub/citydb/modules/kml/database/Queries.java
+++ src/de/tub/citydb/modules/kml/database/Queries.java
@@ -246,7 +246,7 @@ public class Queries {
 			"UNION " + 
 			"SELECT lod4_geometry_id FROM BUILDING " +
 			"WHERE " +
-				"id = ? " +
+				"id = ? AND lod4_geometry_id IS NOT NULL " +
 				"AND id NOT IN " +
 				"(SELECT building_id FROM THEMATIC_SURFACE " +
 					"WHERE building_id = ? " +
@@ -294,7 +294,7 @@ public class Queries {
 			"UNION " + 
 			"SELECT lod3_geometry_id FROM BUILDING " +
 			"WHERE " +
-				"id = ? " +
+				"id = ? AND lod3_geometry_id IS NOT NULL " +
 				"AND id NOT IN " +
 				"(SELECT building_id FROM THEMATIC_SURFACE " +
 					"WHERE building_id = ? " +
@@ -324,7 +324,7 @@ public class Queries {
 			"UNION " + 
 			"SELECT lod2_geometry_id FROM BUILDING " +
 			"WHERE " +
-				"id = ? " +
+				"id = ? AND lod2_geometry_id IS NOT NULL " +
 				"AND id NOT IN " +
 				"(SELECT building_id FROM THEMATIC_SURFACE " +
 					"WHERE building_id = ? " +
